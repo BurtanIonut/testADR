fb8c0a023e4d35f163eb145a1a25fc4b7d120539
BurtanIonut <38885151+BurtanIonut@users.noreply.github.com>
2020-10-10 19:23:06 +0300

    input output from and to webpage

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template, request
+
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    return "Hello World!"
+def form_input():
+    return render_template("textbox_input.html")
+
+
+@app.route('/', methods=['POST'])
+def form_input_post():
+    text = request.form['text']
+    print(text)
+    return render_template("textbox_input.html", data={"text": text})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
